bd6aaf6b0ca57c4f5e7f1c57053ea1886e475474
Mayra <mayragann@gmail.com>
2022-03-17 11:04:42 -0400

    Adjusted comment section of entire file

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-# # reversing a string
+# reversing a string
 
-# from audioop import reverse
+from audioop import reverse
 
 
-# def reversed_word(word):
-#     return word[::-1]
+def reversed_word(word):
+    return word[::-1]
 
-# my_word = reversed_word(input('Enter Word to reverse: '))
+my_word = reversed_word(input('Enter Word to reverse: '))
 
-# print(my_word)
+print(my_word)
 
-# #Capitalize Letters
-# letter = input('What is your favorite thing to do: ')
+#Capitalize Letters
+letter = input('What is your favorite thing to do: ')
 
-# cap_word = letter.title()
-# print (cap_word)
+cap_word = letter.title()
+print (cap_word)
 
-# #compressing a string of characters
+#compressing a string of characters
 
-# def compress(string):
-#     index = 0
-#     compressed = ""
-#     len_str = len(string)
-#     while index != len_str:
-#         count = 1
-#         while (index < len_str-1) and (string[index] == string[index+1]):
-#             count = count + 1
-#             index = index + 1
-#         if count == 1:
-#             compressed += str(string[index])
-#         else:
-#             compressed += str(string[index]) + str(count)
-#         index = index + 1
-#     return compressed
+def compress(string):
+    index = 0
+    compressed = ""
+    len_str = len(string)
+    while index != len_str:
+        count = 1
+        while (index < len_str-1) and (string[index] == string[index+1]):
+            count = count + 1
+            index = index + 1
+        if count == 1:
+            compressed += str(string[index])
+        else:
+            compressed += str(string[index]) + str(count)
+        index = index + 1
+    return compressed
        
  
-# string = "aaaaaaaaaaabbbbbbbbbbbbcccccccccccffffffffffffffjjjjjj"
-# print(compress(string))
+string = "aaaaaaaaaaabbbbbbbbbbbbcccccccccccffffffffffffffjjjjjj"
+print(compress(string))
 
 def is_palindrome():
     def palindrome(word):
